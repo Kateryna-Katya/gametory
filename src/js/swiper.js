36b1bfffb04f69357e61c8d9c8d32efbce93a1f0
Kateryna <katyaukr2014@gmail.com>
2026-02-14 09:31:15 +0200

    add

---
+++ src/js/swiper.js
@@ -0,0 +1,51 @@
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+  modules: [Autoplay],
+
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+  speed: 800,
+  centeredSlides: false,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    1440: {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+      spaceBetween: 40,
+    },
+  },
+});
+
+
+const whySwiper = new Swiper('.why-swiper', {
+  modules: [Autoplay],
+
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 24,
+
+  loop: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    1440: {
+      enabled: false,
+      loop: false,
+      autoplay: false,
+    },
+  },
+});
